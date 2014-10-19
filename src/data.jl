2ff299476a76e5c01ef85c0051994aa39984e0ac
gasagna <lasagnadavide@gmail.com>
2014-10-19 13:29:48 +0100

    Added docstring for DataSet creator function

--- src/data.jl
+++ src/data.jl
@@ -5,13 +5,24 @@ type DataSet
 end
 
 function DataSet(X::Matrix, y::Vector)
-	# X : nfeat x nobs matrix
-	# y : nobs vector
+	# Create DataSet from input data. 
+	# 
+	# Observations are organised in columns in the matrix X, to reflect
+	# the data format required by FANN, which uses an array of pointers
+	# to arrays to store the observations. Hence, we make use of Julia
+	# column-major order to save a copy of the data.
+	# 
+	# Parameters
+	# ----------
+	# X : n_feat x n_obs matrix
+	# y : n_obs vector
+	#
+
 	# checks
 	size(X, 2) == length(y) || error("sizes of X and y do not match")
 
-	num_data = length(y)   # number of examples
-	num_input = size(X, 2) # number of input nodes
+	num_data = length(y)      # number of examples
+	num_input = size(X, 2)    # number of input nodes
 	num_output = size(X, 1)   # number of output nodes
 	
 	# create struct
@@ -19,7 +30,7 @@ function DataSet(X::Matrix, y::Vector)
 			  	             uint32(num_input), 
 				             uint32(num_output))
 
-	# manually change input and output pointers: bad... 
+	# manually change input and output pointers: this is currently wrong 
 	data.input = pointer([pointer(X, i) for i = 1:num_input:length(X)])
 	data.output = pointer([pointer(y, i) for i = 1:length(y)])
 
