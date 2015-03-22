e5f980d44674176f7ce6910819f6bad47fa8d90f
gasagna <lasagnadavide@gmail.com>
2015-03-22 17:52:30 +0000

    removed touch comman from build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -11,14 +11,11 @@ provides(Sources, URI(uri), fannlib, unpacked_dir="fann-master")
 prefix = joinpath(BinDeps.depsdir(fannlib),"usr")
 srcdir = joinpath(BinDeps.depsdir(fannlib),"src", "fann-master") 
 
-copying = joinpath(srcdir, "COPYING.txt")
-
 provides(SimpleBuild,
     (@build_steps begin
         GetSources(fannlib)
         @build_steps begin
             ChangeDirectory(srcdir)
-            `touch $copying`
             `cmake -DCMAKE_INSTALL_PREFIX=$prefix .`
             `make install`
         end
